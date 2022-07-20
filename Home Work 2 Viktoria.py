499a4dd756135ab6b1d340516d59c55aa5fbbec4
ViktoriaHrynko <104625789+ViktoriaHrynko@users.noreply.github.com>
2022-07-20 13:43:04 +0200

    Add files via upload

---
+++ Home Work 2 Viktoria.py
@@ -0,0 +1,90 @@
+# Arithmetic Operators
+x = 15
+y = 10
+print('x+y=', x+y)
+
+print('+-y', x-y)
+
+print('x*y=', x*y)
+
+print('x/y=', x/y)
+
+print('x%y=', x%y)
+
+print('x**y=', x**y)
+
+
+# Comparison Operators
+x=3
+y=7
+print('x>y is',x>y)
+print('x<y is', x<y)
+print('x==y is', x==y)
+print('x !=y is', x !=y)
+print('x >=y is', x>=y)
+
+# Logical Operators
+a = True
+b = False
+print('a and b is', a and b)
+print('a or b is', a or b)
+print('not b is', not b)
+print('not a is', not a)
+
+
+# Bitwise Operators
+# And
+a=10
+b=12
+ans=a&b
+print(ans)
+
+# OR^
+a=10
+b=12
+ans=a|b
+print(ans)
+
+# Left Shift
+var = 2;
+print("var = ", var<<0)
+print("var * 2  = ", var<<1)
+print("var * 8  = ", var<<3)
+
+#Right Shift
+var = 128;
+print("var    = ", var>>0)
+print("var /8   = ", var>>1)
+print("var /4   = ", var>>2)
+
+#~
+var=3
+print('valie=', ~var)
+
+# Identity Operators
+a1=3
+b1=3
+a2="Hello"
+b2="Hello"
+a3=[4,5,6]
+b3=[4,5,6]
+print(a1 is not b1)
+print(a2 is b2)
+print(a3 is b3)
+
+#Membership Operators
+
+a="Vita"
+b={1:'x',2:'y'}
+print('V' in a)
+print('Vita' not in a)
+print(1 in b)
+print(y in b)
+
+
+
+
+
+
+
+
